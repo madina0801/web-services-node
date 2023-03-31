c49a816b15194757f03684cdc8413921dbe76340
Madina <madinatussupova0820@gmail.com>
2023-03-31 10:22:36 +0600

    post data to a server 404 error

--- index.js
+++ index.js
@@ -5,31 +5,43 @@ const PORT = 3000;
 const server = http.createServer();
 
 server.on('request', (req, res) => {
+
+	const friends = [
+		{
+			id: 0,
+			name: 'John',
+		},
+		{
+			id: 1,
+			name: 'Jay',
+		},
+		{
+			id: 2,
+			name: 'Jake',
+		},
+	];
+
 	const items = req.url.split('/');
-	if (items[1] === 'friends') {
+
+	if (req.method === 'GET' && items[1] === 'friends') {
+		console.log(req.method)
 		res.statusCode = 200;
 		res.setHeader('Content-Type', 'application/json');
-		const friends = [
-			{
-				id: 0,
-				name: 'John',
-			},
-			{
-				id: 1,
-				name: 'Jay',
-			},
-			{
-				id: 2,
-				name: 'Jake',
-			},
-		];
 		if (items.length === 3) {
 			const friendIndex = +items[2]
 			res.end(JSON.stringify(friends[friendIndex]));
 		} else {
 			res.end(JSON.stringify(friends));
 		}
-	} else if (items[1] === 'messages') {
+	}
+	if (req.method === 'POST' && items[1] === 'friends') {
+		req.on('data', (data) => {
+			const friend = data.toString();
+			console.log('Request: ', friend);
+			friends.push(JSON.parse(friend));
+		})
+	}
+	if (req.method === 'GET' && items[1] === 'messages') {
 		res.setHeader('Content-Type', 'text/html');
 		res.write('<html>');
 		res.write('<body>');
